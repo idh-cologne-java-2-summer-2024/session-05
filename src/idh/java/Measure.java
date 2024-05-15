75c1e7cf97d949cfcaff301bf68812bfdbbcc1c7
Nils Reiter <nils.reiter@uni-koeln.de>
2024-05-15 10:45:10 +0200

    Clean up

--- src/idh/java/Measure.java
+++ src/idh/java/Measure.java
@@ -10,74 +10,75 @@ import java.util.List;
 import java.util.Random;
 
 public class Measure<T extends List<Integer>> {
-	Random random = new Random(0);
-	int n = (int) Math.pow(10.0, 7.0);
-	int m = 100;
-	OutputStream os = OutputStream.nullOutputStream();
-	OutputStreamWriter osw = new OutputStreamWriter(os);
-	long start;
-	
-	public Measure() {
-		System.out.println("n = " + n);
-	}
-	
-	public void filling(T list) {
-		start = System.currentTimeMillis();
-		for (int i = 0; i < n; i++) {
-			list.add(i);
-		}
-		System.out.println("- fill with " + n + " elements: " + (System.currentTimeMillis()-start) + "ms.");
-	}
-	
-	public void randomAccess(T list) throws IOException {
-		start = System.currentTimeMillis();
-		for (int i = 0; i < m; i++)
-			osw.write(list.get(random.nextInt(n)));
-		System.out.println("- access "+m+" random elements: " + (System.currentTimeMillis()-start) + "ms.");
-	}
-	
-	public void randomRemoval(T list) {
-		start = System.currentTimeMillis();
-		for (int i = 0; i < m; i++)
-			list.remove(random.nextInt(n-i));
-		System.out.println("- remove "+m+" random positions: " + (System.currentTimeMillis()-start) + "ms.");
-	}
-	
-	public void removeInIteration(T list) {
-		start = System.currentTimeMillis();
-		Iterator<Integer> iter = list.listIterator();
-		int removed = 0;
-		while(iter.hasNext()) {
-			iter.next();
-			if (random.nextDouble() < 100/(double)n) {
-				iter.remove();
-				removed++;
-			}
-		}
-		System.out.println("- remove random "+removed+" positions via iterator: " + (System.currentTimeMillis()-start) + "ms.");
+    Random random = new Random(0);
+    int n = (int) Math.pow(10.0, 7.0);
+    int m = 100;
+    OutputStream os = OutputStream.nullOutputStream();
+    OutputStreamWriter osw = new OutputStreamWriter(os);
+    long start;
 
-	}
-	
-	public static void main(String[] args) throws IOException {
-		List<Integer> list = new LinkedList<Integer>();
-		
-		System.out.println("Timing for " + list.getClass().getName());
-		Measure<List<Integer>> m = new Measure<List<Integer>>();
-		m.filling(list);
-		m.randomAccess(list);
-		m.randomRemoval(list);
-		m.removeInIteration(list);
+    public Measure() {
+	System.out.println("This list contains n = " + n + " elements.");
+    }
 
-		
-		list = null;
-		System.gc();
-		
-		list = new ArrayList<Integer>();
-		System.out.println("Timing for " + list.getClass().getName());
-		m = new Measure<List<Integer>>();
-		m.filling(list);
-		m.randomAccess(list);
-		m.randomRemoval(list);
-		m.removeInIteration(list);
+    public void filling(T list) {
+	start = System.currentTimeMillis();
+	for (int i = 0; i < n; i++) {
+	    list.add(random.nextInt());
 	}
+	System.out.println("- fill with " + n + " random elements: " + (System.currentTimeMillis() - start) + "ms.");
+    }
+
+    public void randomAccess(T list) throws IOException {
+	start = System.currentTimeMillis();
+	for (int i = 0; i < m; i++)
+	    osw.write(list.get(random.nextInt(n)));
+	System.out.println("- access " + m + " random elements: " + (System.currentTimeMillis() - start) + "ms.");
+    }
+
+    public void randomRemoval(T list) {
+	start = System.currentTimeMillis();
+	for (int i = 0; i < m; i++)
+	    list.remove(random.nextInt(n - i));
+	System.out.println("- remove " + m + " random positions: " + (System.currentTimeMillis() - start) + "ms.");
+    }
+
+    public void removeInIteration(T list) {
+	start = System.currentTimeMillis();
+	Iterator<Integer> iter = list.listIterator();
+	int removed = 0;
+	double percentage = 0.0001;
+	while (iter.hasNext()) {
+	    iter.next();
+	    if (random.nextDouble() < percentage) {
+		iter.remove();
+		removed++;
+	    }
+	}
+	System.out.println("- remove random " + removed + " positions (" + percentage * 100 + "%) via iterator: "
+		+ (System.currentTimeMillis() - start) + "ms.");
+
+    }
+
+    public static void main(String[] args) throws IOException {
+	List<Integer> list = new LinkedList<Integer>();
+
+	System.out.println("Timing for " + list.getClass().getName());
+	Measure<List<Integer>> m = new Measure<List<Integer>>();
+	m.filling(list);
+	m.randomAccess(list);
+	m.randomRemoval(list);
+	m.removeInIteration(list);
+
+	list = null;
+	System.gc();
+
+	list = new ArrayList<Integer>();
+	System.out.println("Timing for " + list.getClass().getName());
+	m = new Measure<List<Integer>>();
+	m.filling(list);
+	m.randomAccess(list);
+	m.randomRemoval(list);
+	m.removeInIteration(list);
+    }
 }
